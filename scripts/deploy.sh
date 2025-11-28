3356a2f4bdb0d9e1a9212e068dc26fc47085b5b5
Nikita Kakade <81976504+nikitakakade16@users.noreply.github.com>
2025-11-28 18:14:10 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/my-node-app
+docker stop my-node-app
 docker rm -f my-node-app
 docker pull 200901485389.dkr.ecr.us-east-1.amazonaws.com/nikita-ecr-deep:latest
 docker run -p 80:3000 --name my-node-app -d 200901485389.dkr.ecr.us-east-1.amazonaws.com/nikita-ecr-deep:latest
