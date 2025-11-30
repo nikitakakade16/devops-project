26da2137a10341af19cc86fd5ebcec8adecb76ac
Nikita Kakade <81976504+nikitakakade16@users.noreply.github.com>
2025-11-30 17:42:18 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-cd /home/ec2-user/my-node-app
-docker stop my-node-app
-docker rm -f my-node-app
+set -e
+cd /home/ec2-user/my-node-app || true
+aws ecr get-login-password --region us-east-1 \
+ | docker login --username AWS --password-stdin 200901485389.dkr.ecr.us-east-1.amazonaws.com
+docker stop my-node-app || true
+docker rm -f my-node-app || true
 docker pull 200901485389.dkr.ecr.us-east-1.amazonaws.com/nikita-ecr-deep:latest
 docker run -p 80:3000 --name my-node-app -d 200901485389.dkr.ecr.us-east-1.amazonaws.com/nikita-ecr-deep:latest
+echo "Deployment Successful!"
